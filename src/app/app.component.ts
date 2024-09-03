267dcc506448b3f33e4f755336e0f35ea5faeb17
Daniel Sogl <daniel.sogl@thinktecture.com>
2024-09-03 14:23:48 +0200

    add bug reproduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { InputModule } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [InputModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cds-input-readonly-bug';
-}
+export class AppComponent {}
